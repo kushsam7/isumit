adbf072590f919fb05c10e9b58cb17f42d7a7930
kushsam7 <44003087+kushsam7@users.noreply.github.com>
2019-02-07 10:38:25 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,362 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Personal Webite: Sumit Singh</title>
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/font-awesome.css" rel="stylesheet" type="text/css" media="all" />
+<link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen"><!-- chocolate css for gallery light box-->
+<!-- animation -->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<!-- //animation --> 
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- onlinefonts -->
+<link href="//fonts.googleapis.com/css?family=Dancing+Script:400,700" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Yanone+Kaffeesatz:200,300,400,700" rel="stylesheet">
+<!-- //onlinefonts -->
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top"> 
+		<!-- header -->
+		<div class="header-w3layouts"> 
+			<!-- Navigation -->
+			<nav class="navbar navbar-default navbar-fixed-top">
+				<div class="container">
+					<div class="navbar-header page-scroll">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+							<span class="sr-only">Vitae</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<h1><a class="navbar-brand" href="index.html">+ve Vibes, +ve Life.</a></h1>
+					</div> 
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse navbar-ex1-collapse">
+						<ul class="nav navbar-nav navbar-right">
+							<!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+							<li class="hidden"><a class="page-scroll" href="#page-top"></a></li>
+							<li><a class="page-scroll scroll active" href="#home">Home</a></li>
+							<li><a class="page-scroll scroll" href="#about">About</a></li>
+							
+							<li><a class="page-scroll scroll" href="#edu">Education</a></li>
+							<li><a class="page-scroll scroll" href="#exp">Projects</a></li>
+							<li><a class="page-scroll scroll" href="#contact">Contact</a></li>
+						</ul>
+					</div>
+					<!-- /.navbar-collapse -->
+				</div>
+				<!-- /.container -->
+			</nav>  
+		</div>	
+		<!-- //header -->
+		<div class="header jarallax" id="home">
+			<div class="container">
+				<div class="banner-text text-center">
+					<h2>I am Sumit Singh</h2>
+					<h3> - C.E.O. At TeckdR -</h3>
+					<p>All that happens actually happens for good and Do good and good will come to you</p>
+					<div class="w3l-button banner-btn">
+							<a href="#">Download CV</a>
+					</div>
+				</div>	
+				<div class="w3-arrow bounce animated">
+				<a href="#about" class="scroll"><i class="fa fa-angle-down" aria-hidden="true"></i></a>
+			</div>
+			</div>
+			
+		</div>
+		
+	<div class="about" id="about">
+		<div class="container">	
+				<h3 class="w3l-heading">About me</h3>
+			<div class="col-md-4 w3-about-top">
+				<img src="images/3.jpg" class="img-responsive" alt="about-img"/>
+			</div>
+			<div class="col-md-8 w3l-about">
+					<div class="w3ls-heading">
+						<h2>I'm a Student Of M.C.A in Chandigarh University, Chandigarh. I have a passion for web development and also love to develop softwares.</h2>
+					</div>
+					<div class="w3ls-about-info">
+						<p>I am a decent boy from a small town(Gola Gokaran Nath) of Uttar Pradesh,INDIA. I have completed my school life from Children's Academy Gola Gokaran NATH (U.P) . During my school days i was Head Boy of my school. Leadership is my blood skill. So After 10+2 ; i thought about  computer science technology & decided to join software engineering. Goal of my  life is to build up and lead  self  organization and provide software technology for small business organization of INDIA. </p>
+					</div>
+					<div class="w3l-button">
+						<a href="#" data-toggle="modal" data-target="#myModal">check out my profile</a>
+					</div>
+			</div>	
+			<div class="clearfix"></div>
+		</div>
+		
+	</div>
+	<!-- modal -->
+	<div class="modal about-modal fade" id="myModal" tabindex="-1" role="dialog">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header"> 
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
+						<h4 class="modal-title">Info.</h4>
+				</div> 
+				<div class="modal-body">
+					<div class="about"> 
+						<div class="col-md-4 col-sm-4  col-xs-4 about-left ">
+							<img src="images/3.jpg" class="img-responsive" alt=""/>
+						</div>
+						<div class="col-md-8 col-sm-8 col-xs-8 about-right wthree">
+							<h3>Hi, i'm <span>Sumit Singh</span></h3>
+							<h4>Student & Web Developer </h4>
+							<ul class="address">
+								<li>
+									<ul class="agileits-address-text ">
+										<li><b>D.O.B</b></li>
+										<li>17-10-1998</li>
+									</ul>
+								</li>
+								<li>
+									<ul class="agileits-address-text">
+										<li><b>PHONE </b></li>
+										<li>+91 9453658389</li>
+									</ul>
+								</li>
+								<li>
+									<ul class="agileits-address-text">
+										<li><b>ADDRESS </b></li>
+										<li>Le Courbiser Boys Hostel,Chandigarh University</li>
+									</ul>
+								</li>
+								<li>
+									<ul class="agileits-address-text">
+										<li><b>E-MAIL </b></li>
+										<li><a href="mailto:example@mail.com"> kushsam7@gmail.com</a></li>
+									</ul>
+								</li>
+								<li>
+									<ul class="agileits-address-text">
+										<li><b>WEBSITE </b></li>
+										<li><a href="http://w3layouts.com">www.isumit.epizy.com</a></li>
+									</ul>
+								</li>
+							</ul> 
+						</div> 
+						<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+	<!-- //modal -->
+	
+<!--Education-->
+	<div id="edu" class="agile-edu">
+		<div class="container">	
+				<h3 class="w3l-heading">Education</h3>
+			<div class="work-info"> 
+				<div class="col-md-6 col-sm-6 col-xs-6 work-right work-right2"> 
+				</div>
+				<div class="col-md-6  col-sm-6 col-xs-6  work-left work-left2"> 
+					<div class="col-md-6  col-sm-6 col-xs-6  work1">
+						<h4>M.C.A.(Persuing)</h4>
+						<p>Persuing M.C.A. from Chandigarh University,Chandigarh.Best Private University in India.</p></div>
+					<div class="col-md-6  col-sm-6 col-xs-6  work2">
+						<h5>Chandigarh University</h5>
+						<span></span>
+					</div>
+				</div>
+			</div>
+			<div class="work-info"> 
+				<div class="col-md-6  col-sm-6 col-xs-6"> 
+				</div>
+				<div class="col-md-6  col-sm-6 col-xs-6  work-right"> 
+					<div class="col-md-6  col-sm-6 col-xs-6 work2">
+						<span></span>
+						<h5>Invertis University, Bareilly</h5>
+					</div>	
+					<div class="col-md-6  col-sm-6 col-xs-6  work-right1">
+						<h4>B.C.A.</h4>
+						<p>Graduated B.C.A. from Invertis University,Bareilly. Best Private University in Bareilly.</p>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="work-sec-2">
+			<div class="work-info"> 
+				<div class="col-md-6  col-sm-6 col-xs-6  work-right work-right2"> 
+				</div>
+				<div class="col-md-6  col-sm-6 col-xs-6  work-left work-left2 "> 
+					<div class="col-md-6 col-sm-6 col-xs-6  work1">
+						<h4>11th-12th</h4>
+						<p>I completed my 11th-12th from Doon Public School,Mohammdi.Best School in Mohammdi Kheri.</p></div>
+					<div class="col-md-6  col-sm-6 col-xs-6  work2">
+						<h5> Doon Public School, Mohammdi Kheri</h5>
+						<span></span>
+					</div>
+				</div>
+			</div>
+			<div class="work-info"> 
+				<div class="col-md-6  col-sm-6 col-xs-6 "> 
+				</div>
+				<div class="col-md-6  col-sm-6 col-xs-6  work-right"> 
+					<div class="col-md-6  col-sm-6 col-xs-6  work2">
+						<span></span>
+						<h5>Children's Academy, Gola Gokaran Nath</h5>
+					</div>	
+					<div class="col-md-6 col-sm-6 col-xs-6  work-right1">
+						<h4>Nursery-10th </h4>
+						<p>I started my studies from Childrens Academy and I learnt many things from here.Best School in Gola Gokaran Nath.</p> </div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<span class="edu-circle"></span>
+			</div>
+		</div>
+	</div>
+<!--//Education-->
+<div class="agile-border">
+	<span></span>
+</div>
+<!-- work-experience-->
+	<div id="exp" class="agile-edu exp">
+		<div class="container">	
+				<h3 class="w3l-heading">Projects</h3>
+			<div class="work-info"> 
+				<div class="col-md-6  col-sm-6 col-xs-6  work-right work-right2"> 
+				</div>
+				<div class="col-md-6  col-sm-6 col-xs-6  work-left work-left2"> 
+					<div class="col-md-6  col-sm-6 col-xs-6  work1">
+						<h4>Doctor's Helpline</h4>
+						<p>I developed a website for my district. Doctor's Helpline helps you to easily find the Doctors and closest healthcare center. Using HTML,CSS,JS,PHP</p></div>
+					<div class="col-md-6  col-sm-6 col-xs-6 work2">
+						<h5> Website</h5>
+						<span></span>
+					</div>
+				</div>
+			</div>
+			<div class="work-info"> 
+				<div class="col-md-6  col-sm-6 col-xs-6 "> 
+				</div>
+				<div class="col-md-6  col-sm-6 col-xs-6 work-right"> 
+					<div class="col-md-6 col-sm-6 col-xs-6 work2">
+						<span></span>
+						<h5>Software</h5>
+					</div>	
+					<div class="col-md-6  col-sm-6 col-xs-6 work-right1">
+						<h4>Medical Inventory Management System</h4>
+						<p>We developed a major project in B.C.A. for managing inventory in a mediacl store with my project partners Satyam Gupta, Ayushi Dixit, Nishu Yadav using C#,Mysql in the guidance of Mr.Kushal Sir and Mr.Taneja Sir.</p>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="work-2">
+			<div class="work-info"> 
+				<div class="col-md-6  col-sm-6 col-xs-6 work-right work-right2"> 
+				</div>
+				<div class="col-md-6  col-sm-6 col-xs-6  work-left work-left2 "> 
+					<div class="col-md-6  col-sm-6 col-xs-6 work1">
+						<h4>Tetris</h4>
+						<p>we developed a tetris game for entertainment with my project partners Prashant Kumar and Shobhit Mishra in the guidance of Mr. Ashosk Sir.</p></div>
+					<div class="col-md-6  col-sm-6 col-xs-6  work2">
+						<h5>Console Game</h5>
+						<span></span>
+					</div>
+				</div>
+			</div>
+			<div class="work-info"> 
+				<div class="col-md-6  col-sm-6 col-xs-6 "> 
+				</div>
+				<div class="col-md-6  col-sm-6 col-xs-6 work-right"> 
+					<div class="col-md-6  col-sm-6 col-xs-6 work2">
+						<span></span>
+						<h5>Website</h5>
+					</div>	
+					<div class="col-md-6  col-sm-6 col-xs-6 work-right1">
+						<h4>Bharat Seeds Corporation</h4>
+						<p>I completed this project for a company Bharat Seeds Corporation to easily provide a information to the customers  using HTML,CSS,JS,PHP.</p> </div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<span class="edu-circle"></span>
+		</div>
+	</div>
+</div>
+<!--//work-experience-->
+<!-- contact -->
+<div id="contact" class="w3_agileits-contact">
+		<div class="container">	
+				<h3 class="w3l-heading">contact</h3>
+				<div class="w3_agile-contact  jarallax text-center">
+					<form action="#" method="get">
+						<input type="text" placeholder="Name" name="name" class="name" required>
+						<input type="text" placeholder="Phone Number" name="phone" class="name" required>
+						<input type="email" placeholder="Email" name="email" class="email" required>
+						<textarea name="message" placeholder="Message"></textarea>
+						<input type="submit" value="send">
+					</form>
+					<h4>or</h4>
+					<a href="mailto:info@example.com">kushsam7@gmail.com</a>
+					<h5>+91 9453658389</h5>
+					<ul class="social-icons-w3_agile">
+						<li><a href="www.facebook.com/kushsam7" class="fa fa-facebook icon icon-border facebook"> </a></li>
+                      <li><a href="https://www.instagram.com/______sumit/" class="fa fa-instagram icon icon-border instagram"> </a></li>
+						<li><a href="www.facebook.com/beingsumit7" class="fa fa-twitter icon icon-border twitter"> </a></li>
+						
+					</ul>
+				</div>
+		</div>	
+</div>	
+<!-- footer -->
+<div class="agileits_w3layouts-footer text-center">
+ 
+	<div class="container">
+       
+		<p>&copy; 2018 Personal Website. All rights reserved | Design by <a href="http://www.isumit.epizy.com">Su Mit</a></p>
+       <img src='https://www.counter12.com/img-yy8bA1y4ZA8aZ56c-2.gif' border='0' style="width:90px;height:30px;"><script type='text/javascript' src='https://www.counter12.com/ad.js?id=yy8bA1y4ZA8aZ56c'></script>
+	</div>
+</div>	
+<!-- //footer -->	
+<!-- //contact -->
+<!-- PopUp-Box-JavaScript -->
+		<script src="js/jquery.chocolat.js"></script>
+		<script type="text/javascript">
+			$(function() {
+				$('.filtr-item a').Chocolat();
+			});
+		</script>
+	<!-- //PopUp-Box-JavaScript -->	
+	<!-- fliter-JavaScript -->
+		<script src="js/jquery.filterizr.js"></script>
+		<script src="js/controls.js"></script>
+		<script type="text/javascript">
+			$(function() {
+				$('.filtr-container').filterizr();
+			});
+		</script>
+	<!-- //fliter-JavaScript -->
+<script src="js/jarallax.js"></script>
+<script src="js/SmoothScroll.min.js"></script>
+<script type="text/javascript">
+	/* init Jarallax */
+	$('.jarallax').jarallax({
+		speed: 0.5,
+		imgWidth: 1366,
+		imgHeight: 768
+	})
+</script>
+<!-- start-smoth-scrolling -->
+	<script type="text/javascript" src="js/easing.js"></script>
+<!-- //here ends scrolling icon -->
+	
+
+</body>
+</html>
